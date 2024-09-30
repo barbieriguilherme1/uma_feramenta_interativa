4124d8752f92c3fa0cc6cb8ecbb15908f4a0e4b8
guilherme <144943817+barbieriguilherme1@users.noreply.github.com>
2024-09-30 11:38:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Brasil agro',
+    'Qual o tamanho do agro Brasileiro?',
+    ' Cerca de US$ 148,58 bilhões '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Agricultura junto com tencologia',
+    'qual o tamanho da tecnologia do agricultura?',
+    'O uso de software sofisticado na,agricultura ajuda a reduzir o consumo de insumos,fertilizantes e pesticidas.'
+)
+
+criaCartao(
+    'Brasil bovinos',
+    'Qual o tamanho rebanho bovino brasileiro?',
+    '234,4 milhões de animais em 2022'
+)
